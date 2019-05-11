949c503ee329749ae347294e2f91a3a5033b1bce
vnepveu <victor.nepveu@imt-atlantique.net>
2019-05-11 11:21:54 +0200

    Improve user-friendliness
    - Can correct previous annotation
    - Can use the arrow keys to annotate

--- annotation_software/main.py
+++ annotation_software/main.py
@@ -18,7 +18,7 @@ while "face-database" not in os.listdir(directory):
     showerror("Wrong location", "Make sure the selected folder contains 'face-database'")
     directory = askdirectory(title="Locate the folder containing 'face-database'")
 directory = os.path.join(directory, "face-database")
-print(directory)
+print("face-database folder found in {}".format(directory))
 
 preselection_file = "test-set.csv"
 
@@ -30,7 +30,6 @@ try:
         lines = fp.readlines()
         number_of_pictures = 0
         for line in lines:
-            print(line)
             # The line is under in the format "name, n" where n is the number of picture in the corresponding folder
             name = line.split(",")[0].strip()
             try:
@@ -58,7 +57,24 @@ class MyApp:
         self.update_photo()
         # Remove absolute path
         current_picture = os.path.normpath(self.current_filepath.replace(directory, ""))
+        print(button_value)
         self.results[current_picture] = button_value
+
+    # Alias of the above function to ignore the event argument
+    def validate_keyboard(self, button_value, event):
+        self.validate(button_value)
+
+    def go_back(self):
+        self.current_picture_number -= 1
+        self.current_name, self.current_filepath = list_of_pictures[self.current_picture_number]
+        image = Image.open(self.current_filepath)
+        h, w = image.height, image.width
+        ratio = w / h
+        image = image.resize((int(round(800 * ratio)), 800))
+        photo = ImageTk.PhotoImage(image)
+        self.image.configure(image=photo)
+        self.image.image = photo
+        self.update_text()
 
     def update_photo(self, quit_and_save=False):
         """
@@ -82,6 +98,8 @@ class MyApp:
             showinfo("Save and quit", "Saving the results under results.pkl and quitting")
             self.results["last_picture_treated"] = self.current_picture_number
             file_save(self.results)
+            for k,v in self.results.items() :
+                print(k, v)
             print("Results saved")
             self.parent.quit()
 
@@ -95,6 +113,7 @@ class MyApp:
         Update the text displayed, to show the next ticket pair
         :return: None
         """
+
         self.text.config(text="Est-ce que cette photo représente bien {}".format(self.current_name))
 
     def __init__(self, parent):
@@ -117,21 +136,30 @@ class MyApp:
         quit_and_save = tk.Button(self.parent, text="Quit and save", command=partial(self.update_photo, True))
         quit_and_save.grid(row=2, column=1)
 
+        # Add "return" button
+        go_back = tk.Button(self.parent, text="Retour à la photo précédente", command=self.go_back)
+        go_back.grid(row=2, column=2)
+
         #  Configure image and buttons widgets
         self.image = tk.Label()
-        self.image.grid(row=0, column=0, columnspan=3)
-        true = tk.Button(self.parent, text="Oui", command=partial(self.validate, True))
-        false = tk.Button(self.parent, text="Non", command=partial(self.validate, False))
+        self.image.grid(row=0, column=0, columnspan=4)
+        true = tk.Button(self.parent, text="Oui \n (Flèche gauche)", command=partial(self.validate, True))
+        false = tk.Button(self.parent, text="Non \n (Flèche droite)", command=partial(self.validate, False))
         true.grid(row=2, column=0, columnspan=1, sticky="nswe")
-        false.grid(row=2, column=2, columnspan=1, sticky="nswe")
+        false.grid(row=2, column=3, columnspan=1, sticky="nswe")
         self.parent.columnconfigure(0, weight=1)
         self.parent.columnconfigure(1, weight=1)
         self.parent.columnconfigure(2, weight=1)
+        self.parent.columnconfigure(3, weight=1)
+
+        # Add arrow keys binding
+        self.parent.bind("<Left>", partial(self.validate_keyboard, True))
+        self.parent.bind("<Right>", partial(self.validate_keyboard, False))
 
         # Configure text widget
         self.text = tk.Label(parent, text="Merci de cliquer sur un des deux boutons pour commencer")
         self.text.configure(anchor="center")
-        self.text.grid(row=1, column=0, columnspan=2)
+        self.text.grid(row=1, column=1, columnspan=2)
 
         # Load previous results to resume where we stopped
         try:
